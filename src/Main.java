2d8c7616dced2f8eb823ccdb0f5504ead9777b6c
lipov_anton1001 <lopov_anton1001@ukr.net>
2025-11-16 17:07:43 +0200

    update

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Lab-4!");
+        System.out.println("Build and test have finished..");
+    }
+    
+    public static String getMessage() {
+        return "Hello from Lab-4!";
+    }
+}
---
+++ test/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    
+    @Test
+    public void testGetMessage() {
+        String result = Main.getMessage();
+        assertEquals("Hello from Lab-4!", result);
+    }
+    
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
