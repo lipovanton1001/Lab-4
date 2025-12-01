03b81bf2f72027912a553b4b1377474592466c7d
lipov_anton1001 <lopov_anton1001@ukr.net>
2025-12-01 11:18:30 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Lab-4!");
+        System.out.println("Hello from Lab-4");
         System.out.println("Build and test have finished..");
     }
     
